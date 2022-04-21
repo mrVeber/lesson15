2ddf24ddc828ddcf1dfb25c7a557e9d1c5fa4f10
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-21 10:38:05 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ task1/ArithProgression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class ArithProgression : ISeries
+    {
+        int stepArif;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue += stepArif;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int x)
+        {
+            stepArif = x;
+        }
+    }
+
+}
---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.setStart(12);
+            arithProgression.setStep(3);
+            Console.WriteLine("Арифметическая прогрессия:");
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+
+            arithProgression.reset();
+            Console.WriteLine("Сброс и изменение шага:");
+            arithProgression.setStep(4);
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.setStart(10);
+            geomProgression.setStep(2);
+
+            Console.WriteLine("\nГеометрическая прогрессия:");
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+
+            geomProgression.reset();
+            Console.WriteLine("Сброс и изменение шага:");
+            geomProgression.setStep(4);
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            Console.ReadKey();
+        }
+    }
+}
